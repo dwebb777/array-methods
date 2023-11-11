44cd21b83a93592cc53ed04a7a92978b401db658
David Webb <webbd5050@gmail.com>
2023-11-11 13:07:05 -0500

    easy??

--- exercises/e9.js
+++ exercises/e9.js
@@ -1,14 +1,12 @@
-import { data } from "../data/data";
+import { data } from "../data/data"
 
 // SPACE DATA EXERCISE 9
 // Return the data object of the planet "Earth"
 // Return example: { key1: value1, key2: value2, ... , keyN: valueN }
 
 export function getEarthData(data) {
-  // Your code goes here...
+  return data.planets[6]
 }
-
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-9"
