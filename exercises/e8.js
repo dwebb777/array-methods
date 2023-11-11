deee0097b4c9757c0183a3e3edfc1cf66f224148
David Webb <webbd5050@gmail.com>
2023-11-11 13:04:17 -0500

    a little trickier!

--- exercises/e8.js
+++ exercises/e8.js
@@ -1,14 +1,22 @@
-import { data } from "../data/data";
+import { data } from "../data/data"
 
 // SPACE DATA EXERCISE 8
 // Return a Planet name by a given moon name
 // Return example: 'Planet Name'
 
 export function findPlanetNameByMoon(data, moonName) {
-  // Your code goes here...
+  let planetWithMoon = ""
+  for (let i = 0; i < data.planets.length; i++) {
+    if (data.planets[i].moons) {
+      for (let j = 0; j < data.planets[i].moons.length; j++) {
+        if (data.planets[i].moons[j] == moonName) {
+          planetWithMoon += data.planets[i].name
+        }
+      }
+    } else false
+  }
+  return planetWithMoon
 }
-
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-8"
