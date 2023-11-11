40a7901ddb6adfef48d616b92a472674f39f8746
David Webb <webbd5050@gmail.com>
2023-11-11 12:50:12 -0500

    number 7 is complete!

--- exercises/e7.js
+++ exercises/e7.js
@@ -1,14 +1,18 @@
-import { data } from "../data/data";
+import { data } from "../data/data"
 
 // SPACE DATA EXERCISE 7
 // Return an array of all Planets names that have moons
 // Return example: ['name1', 'name2', ... , 'nameN']
 
 export function getPlanetsNamesWithMoons(data) {
-  // Your code goes here...
+  let haveMoons = []
+  for (let i = 0; i < data.planets.length; i++) {
+    if (data.planets[i].moonsCount) {
+      haveMoons.push(data.planets[i].name)
+    }
+  }
+  return haveMoons
 }
-
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-7"
