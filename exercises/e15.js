e720a749b86db362b473d210b51c8fea22b5a4e4
David Webb <webbd5050@gmail.com>
2023-11-16 12:58:36 -0500

    no problem

--- exercises/e15.js
+++ exercises/e15.js
@@ -1,14 +1,18 @@
-import { data } from "../data/data";
+import { data } from "../data/data"
 
 // SPACE DATA EXERCISE 15
 // Return an array of Planets' names without moons
 // Return example: ['name1', 'name2', ... , 'nameN']
 
 export function getPlanetsWithNoMoons(data) {
-  // Your code goes here...
+  let noMoons = []
+  for (let i = 0; i < data.planets.length; i++) {
+    if (!data.planets[i].moonsCount) {
+      noMoons.push(data.planets[i].name)
+    }
+  }
+  return noMoons
 }
-
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-15"
