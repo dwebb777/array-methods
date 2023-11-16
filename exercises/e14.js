193f43877d887ef3bf1004cb8de2c3fefc71b0d2
David Webb <webbd5050@gmail.com>
2023-11-16 12:55:29 -0500

    easy

--- exercises/e14.js
+++ exercises/e14.js
@@ -1,14 +1,16 @@
-import { data } from "../data/data";
+import { data } from "../data/data"
 
 // SPACE DATA EXERCISE 14
 // Return the sum of orbital periods of all asteroids
 // Return example: 234234.234
 
 export function getOrbitalPeriodsSum(data) {
-  // Your code goes here...
+  let sum = 0
+  for (let i = 0; i < data.asteroids.length; i++) {
+    sum += data.asteroids[i].orbitalPeriod
+  }
+  return sum
 }
-
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-14"
