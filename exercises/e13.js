3bb1b6acafcc547c1a72e55ae27332e2078f5a23
David Webb <webbd5050@gmail.com>
2023-11-16 12:51:54 -0500

    another one completed!

--- exercises/e13.js
+++ exercises/e13.js
@@ -1,14 +1,18 @@
-import { data } from "../data/data";
+import { data } from "../data/data"
 
 // SPACE DATA EXERCISE 13
 // Return the average temperature of all the Planets
 // Return example: 132.53
 
 export function getAveragePlanetsTemperature(data) {
-  // Your code goes here...
+  let avgTempAll
+  let sum = 0
+  for (let i = 0; i < data.planets.length; i++) {
+    sum += data.planets[i].avgTemp
+  }
+  avgTempAll = sum / data.planets.length
+  return avgTempAll
 }
-
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-13"
